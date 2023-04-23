3f3d525c5fbd09bd037e2c7f4ddd26d7f88b47f6
Tấn Minh Lương <tanminhluong99@gmail.com>
2023-04-23 10:02:37 +0700

    ...

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -101,7 +101,7 @@ const Login = () => {
         </Link>{" "}
         to join and share with the community
       </p>
-      {/* {error && (
+      {error && (
         <Toast className="mx-auto mt-10">
           <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
             <HiExclamation className="h-5 w-5" />
@@ -111,7 +111,7 @@ const Login = () => {
           </div>
           <Toast.Toggle />
         </Toast>
-      )} */}
+      )}
     </section>
   );
 };
